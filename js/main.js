2ad35a39d8bab78ea6e4b6602478ae66d414ffef
Jill Blaeser <jblaeser01@gmail.com>
2019-05-12 16:51:41 -0500

    Set up v-for to get questions

--- js/main.js
+++ js/main.js
@@ -1,33 +1,41 @@
 var quiz = {
-    easy: [
+    questions: [
         {question: "What do all calico cats have in common?",
         answers: [
             {a: "They are male."},
             {b: "They have short hair."},
             {c: "They are female.", correct: true},
             {d: "They have long hair."}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "Which plant produces an effect similar to LSD or marijuana in cats?",
         answers: [
             {a: "Catnip", correct: true},
             {b: "Lillies"},
             {c: "Spinach"},
             {d: "Oregano"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "Which of these foods should your cat never eat?",
         answers: [
             {a: "Grapes"},
             {b: "Garlic"},
             {c: "Chocolate"},
             {d: "All of the Above", correct: true}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "Most cats cannot actually digest what food?",
         answers: [
             {a: "Tuna"},
             {b: "Milk", correct: true},
             {c: "Chicken"},
             {d: "Mice"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "How far can cats jump?",
         answers: [
             {a: "10 times their length"},
@@ -41,399 +49,516 @@ var quiz = {
             {b: "Luring Prey"},
             {c: "Their Own Entertainment"},
             {d: "Greeting Other Cats"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "Cats make very little knows when walking due to what?",
         answers: [
             {a: "Their Size"},
             {b: "Soft Pads on Their Feet"},
             {c: "Furry Feet"},
             {d: "Cat Magic"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "What do cats use their whiskers for?",
         answers: [
             {a: "Smelling"},
             {b: "Tasting Food"},
             {c: "Balance"},
             {d: "Determing What Small Spaces They Can Fit In", correct: true}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "How many toes does a normal cat have?",
         answers: [
             {a: "18", correct: true},
             {b: "20"},
             {c: "24"},
             {d: "16"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "When are cats most active?",
         answers: [
             {a: "Night"},
             {b: "Afternoon"},
             {c: "Dawn and Dusk", correct: true},
             {d: "Mid-Morning"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "What do cats spend up to a third of their waking hours doing?",
         answers: [
             {a: "Eating"},
             {b: "Playing"},
             {c: "Staring Into Space"},
             {d: "Grooming", correct: true}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "What is a group of adult cats called?",
         answers: [
             {a: "Pride"},
             {b: "Clowder", correct: true},
             {c: "Pack"},
             {d: "Catlet"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "How much of its life does the average cat spend sleeping?",
         answers: [
             {a: "90%"},
             {b: "60%"},
             {c: "33%"},
             {d: "75%", correct: true}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "What is one of the longest running Broadway shows of all time?",
         answers: [
             {a: "Cat on a Hot Tin Roof"},
             {b: "Cats", correct: true},
             {c: "The Aristocats"},
             {d: "The Cat's Meow"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "How fast can the average cat run?",
         answers: [
             {a: "15mph"},
             {b: "50mph"},
             {c: "30mph", correct: true},
             {d: "5mph"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "What kind of cat is believed to be good luck in Great Britain and Germany?",
         answers: [
             {a: "Black Cat", correct: true},
             {b: "Orange Cat"},
             {c: "Female Cat"},
             {d: "Tabby Cat"}
-        ]},
+        ],
+        level: "easy"
+        },
         {question: "Which ancient civilization worshipped cats?",
         answers: [
             {a: "Romans"},
             {b: "Babylonians"},
             {c: "Byzantines"},
             {d: "Egyptians", correct: true}
-        ]},
-    ],
-    medium: [
+        ],
+        level: "easy"
+        },
         {question: "How many muscles control a cat's ears?",
         answers: [
             {a: "13"},
             {b: "32", correct: true},
             {c: "7"},
             {d: "46"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "What are the tufts of hair inside a cat's ears called?",
         answers: [
             {a: "Ear Tufts"},
             {b: "Ear Furs"},
             {c: "Ear Furnishings", correct: true},
             {d: "Ear Padding"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "Which was the first cartoon cat?",
         answers: [
             {a: "Garfield"},
             {b: "Felix", correct: true},
             {c: "Tom of Tom & Jerry"},
             {d: "Sylvester"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "What do adult cats only do to humans?",
         answers: [
             {a: "Groom Them"},
             {b: "Scratch Them"},
             {c: "Meow at Them", correct: true},
             {d: "Sleep on Them"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "White cats with blue eyes are more likely to be what?",
         answers: [
             {a: "Deaf", correct: true},
             {b: "Blind"},
             {c: "Overweight"},
             {d: "Mute"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "Hemingway Cats have what?",
         answers: [
             {a: "A Drinking Problem"},
             {b: "A Best-Selling Novel"},
             {c: "Extra Toes", correct: true},
             {d: "Smokey Grey Fur"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "What was Bill Clinton's famous cat named?",
         answers: [
             {a: "Boots"},
             {b: "Mr. Whiskers"},
             {c: "Vice Purrsident Al Grr"},
             {d: "Socks", correct: true}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "Purring can do what?",
         answers: [
             {a: "Improve Bone Density"},
             {b: "Lower Blood Pressure"},
             {c: "Promote Healing"},
             {d: "All of the Above", correct: true}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "Cats are the only mammal that cannot taste what?",
         answers: [
             {a: "Salty"},
             {b: "Sweet", correct: true},
             {c: "Bitter"},
             {d: "Umami"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "What is the most popular breed of cat in the United States?",
         answers: [
             {a: "Siamese"},
             {b: "Himalayan"},
             {c: "Exotic Shorthair", correct: true},
             {d: "Ragdoll"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "What is a cat breeder called?",
         answers: [
             {a: "Cattery", correct: true},
             {b: "Cattle"},
             {c: "Cat House"},
             {d: "Felinery"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "What can cats do in order to survive?",
         answers: [
             {a: "Shed their tails"},
             {b: "Drink Sea Water", correct: true},
             {c: "Hibernate"},
             {d: "Play Dead"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "How many species have gone extinct at the claws of cats?",
         answers: [
             {a: "103"},
             {b: "11"},
             {c: "64"},
             {d: "33", correct: true}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "Much like human fingerprints, each cat has a unique what?",
         answers: [
             {a: "Paw Print"},
             {b: "Nose Print", correct: true},
             {c: "Coat Pattern"},
             {d: "Ear Shape"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "Which plant produces an effect similar to LSD or marijuana in cats",
         answers: [
             {a: "Catnip", correct: true},
             {b: "Lillies"},
             {c: "Spinach"},
             {d: "Oregano"}
-        ]},
+        ],
+        level: "medium"
+        },
         {question: "When were cats first domesticated?",
         answers: [
             {a: "1000 BC"},
             {b: "100 AD"},
             {c: "2500 BC"},
             {d: "7500 BC", correct: true}
-        ]},
-    ],
-    hard: [
+        ],
+        level: "medium"
+        },
         {question: "What is the technical term for a hairball?",
         answers: [
             {a: "Trichobezoar", correct: true},
             {b: "Agnail"},
             {c: "Pityriasis"},
             {d: "Canities"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "How much of their genome do domestic cats share with tigers?",
         answers: [
-            {a: "85.7%",
+            {a: "85.7%"},
             {b: "98.2%"},
             {c: "95.6%", correct: true},
             {d: "93.8%"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "What is a group of kittens called?",
         answers: [
             {a: "Litter"},
             {b: "Kindle", correct: true},
             {c: "Clutch"},
             {d: "Collection"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "What does it mean if your cat's tail is straight and vibrating?",
         answers: [
             {a: "It's Scared"},
             {b: "It's Happy to See You", correct: true},
             {c: "It's Angry"},
             {d: "It's Tired"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "Male cats are more likely to be what?",
         answers: [
             {a: "Black"},
             {b: "Long-Haired"},
             {c: "Left-Pawed", correct: true},
             {d: "Good-Natured"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "At what age can a female cat become pregnant?",
         answers: [
             {a: "4 Months", correct: true},
             {b: "1 Year"},
             {c: "9 Months"},
             {d: "2 Years"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "What group of people are more likely to own a cat than a dog?",
         answers: [
             {a: "Men"},
             {b: "Brunettes"},
             {c: "Only Children"},
             {d: "College Graduates", correct: true}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "What do Turkish Vans, Maine Coons, and Bengals all typically enjoy?",
         answers: [
             {a: "Dogs"},
             {b: "Play Fetch"},
             {c: "Water", correct: true},
             {d: "Digging Holes"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "What location is home to more than 200 feral cats?",
         answers: [
             {a: "Machu Pichu"},
             {b: "Disneyland", correct: true},
             {c: "Taj Mahal"},
             {d: "Westminster Abbey"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "How did Nikola Tesla's cat inspire him to investigate electricity?",
         answers: [
             {a: "It shocked him", correct: true},
             {b: "It had static in its fur"},
             {c: "It electrocuted a mouse"},
             {d: "Its hair stood on end when it was scared"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "Owning a cat can reduce the risk of what by a third?",
         answers: [
             {a: "Dementia"},
             {b: "Alcoholism"},
             {c: "Arthritis"},
             {d: "Stroke", correct: true}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "What was the punishment for smuggling a cat out of ancient Egypt?",
         answers: [
             {a: "Hand Cut Off"},
             {b: "Death", correct: true},
             {c: "Public Whipping"},
             {d: "30 Days in Jail"}
-        ]},
+        ],
+        level: "hard"
+        },
         {question: "When a family cat died in ancient Egypt, how did family members mourn?",
         answers: [
             {a: "Cut Their Hair"},
             {b: "Wore Black Eyeliner"},
             {c: "Shaved Their Eyebrows", correct: true},
             {d: "Dressed in Furs"}
-        ]},
-    ],
-    extraHard: [
+        ],
+        level: "hard"
+        },
         {question: "What was the name of the first cat launched into space? (Don't worry, it survived!)",
         answers: [
             {a: "Amelie"},
             {b: "Felicette", correct: true},
             {c: "Chaussette"},
             {d: "Petit Choux"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "How many bones does a cat have?",
         answers: [
             {a: "244", correct: true},
-            {b: "266",
+            {b: "266"},
             {c: "338"},
             {d: "402"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "Who made the first cat video?",
         answers: [
             {a: "Nikola Tesla"},
             {b: "Walt Disney"},
             {c: "Auguste Lumiere"},
             {d: "Thomas Edison", correct: true}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "What trait do only cats, camels, and giraffes share?",
         answers: [
             {a: "They have sand-paper textured tongues."},
             {b: "They have a different number of toes on their front and back feet."},
             {c: "They move both right feet first, then both left feet.", correct: true},
             {d: "Their skin is not colored the same as their fur."}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "An ambassador from which country used his Siamese cats to detect hidden microphones in his walls?",
         answers: [
             {a: "Japan"},
             {b: "Guyana"},
             {c: "The Netherlands", correct: true},
             {d: "Azerbaijan"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "What was composer David Teie's album of songs based on feline communication and sounds that pique the interest of cats called?",
         answers: [
             {a: "Music for Cats", correct: true},
             {b: "Cats: The Musical"},
             {c: "Songs About Cats in A Minor"},
             {d: "Meow Cats What I Call Mewsic"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "Whose wife, when asked if her husband had any hobbies, responded with 'Cats!'?",
         answers: [
             {a: "Dean Martin"},
             {b: "Abraham Lincoln", correct: true},
             {c: "Alfred Hitchcock"},
             {d: "Winston Churchill"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "In 2015a 6 x 8.5 foot oil painting billed as the 'world's largest cat painting' sold at auction for more than $820,000. What is it called?",
         answers: [
             {a: "Feline a Deux"},
-            {b: "Puss Napping",
+            {b: "Puss Napping"},
             {c: "A Chop for One"},
             {d: "My Wife's Lovers", correct: true}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "In the 1870s, Liege Belgium tried to train 37 cats to do what?",
         answers: [
             {a: "Walk People Home"},
             {b: "Ring a Fire Alarm When They Smelled Smoke"},
             {c: "Deliver Mail", correct: true},
             {d: "Catch Pick Pocketers"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "Macskaköröm is a Hungarian word that literally translates to 'cat claws.' What does it refer to?",
         answers: [
             {a: "Quotation Marks", correct: true},
             {b: "A Type of Pastry"},
             {c: "Gossiping Behind Someone's Back"},
             {d: "A Hungarian Folk Dance"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "A cat of what odd color was born in Denmark in 1995?",
         answers: [
             {a: "Blue"},
             {b: "Green", correct: true},
             {c: "Pink"},
             {d: "Purple"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "Stubbs, an orange tabby, was mayor of what town for 20 years?",
         answers: [
             {a: "Idyllwild, California"},
             {b: "Cormorant Village, Vermont"},
             {c: "Talkeetna, Alaska", correct: true},
             {d: "Lajitas, Texas"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "Which wild cat did the domestic cat diverge from approximately 9000 years ago?",
         answers: [
             {a: "Servals"},
             {b: "Near Eastern Wildcat", correct: true},
             {c: "Savannah Cat"},
             {d: "Bobcat"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "Pope Innocent VIII condemned cats as evil during what period in time?",
         answers: [
             {a: "The Spanish Inquisition", correct: true},
             {b: "The Black Plague"},
             {c: "The Holocaust"},
             {d: "The Crusades"}
-        ]},
+        ],
+        level: "extraHard"
+        },
         {question: "What was the name of Isaac Newton's cat for whom he invented the cat flap in order to keep it from opening the door and ruining his light experiments?",
         answers: [
             {a: "Perruque"},
             {b: "Macak"},
             {c: "Childebrand"},
             {d: "Spithead", correct: true}
-        ]},
-
-        
+        ],
+        level: "extraHard"
+        },       
     ]
-}
+};
+
+new Vue({
+    el: '#app',
+    data: {
+        quiz: quiz,
+        questionIndex: 0,
+    },
+    methods: {
+        submit: function() {
+            
+        }
+    }
+})
