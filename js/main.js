d495f5a298ece9f8c9df6c26f2eeb470d8a446af
Jill Blaeser <jblaeser01@gmail.com>
2019-05-11 19:49:26 -0500

    Easy questions setup

---
+++ js/main.js
@@ -0,0 +1,126 @@
+var quiz = {
+    easy: [
+        {question: "What do all calico cats have in common?",
+        answers: [
+            {a: "They are male."},
+            {b: "They have short hair."},
+            {c: "They are female.", correct: true},
+            {d: "They have long hair."}
+        ]},
+        {question: "Which plant produces an effect similar to LSD or marijuana in cats?",
+        answers: [
+            {a: "Catnip", correct: true},
+            {b: "Lillies"},
+            {c: "Spinach"},
+            {d: "Oregano"}
+        ]},
+        {question: "Which of these foods should your cat never eat?",
+        answers: [
+            {a: "Grapes"},
+            {b: "Garlic"},
+            {c: "Chocolate"},
+            {d: "All of the Above", correct: true}
+        ]},
+        {question: "Most cats cannot actually digest what food?",
+        answers: [
+            {a: "Tuna"},
+            {b: "Milk", correct: true},
+            {c: "Chicken"},
+            {d: "Mice"}
+        ]},
+        {question: "How far can cats jump?",
+        answers: [
+            {a: "10 times their length"},
+            {b: "3 times their length"},
+            {c: "8 times their length"},
+            {d: "6 times their length", correct: true}
+        ]},
+        {question: "What do cats use their tails for?",
+        answers: [
+            {a: "Balance", correct: true},
+            {b: "Luring Prey"},
+            {c: "Their Own Entertainment"},
+            {d: "Greeting Other Cats"}
+        ]},
+        {question: "Cats make very little knows when walking due to what?",
+        answers: [
+            {a: "Their Size"},
+            {b: "Soft Pads on Their Feet"},
+            {c: "Furry Feet"},
+            {d: "Cat Magic"}
+        ]},
+        {question: "What do cats use their whiskers for?",
+        answers: [
+            {a: "Smelling"},
+            {b: "Tasting Food"},
+            {c: "Balance"},
+            {d: "Determing What Small Spaces They Can Fit In", correct: true}
+        ]},
+        {question: "How many toes does a normal cat have?",
+        answers: [
+            {a: "18", correct: true},
+            {b: "20"},
+            {c: "24"},
+            {d: "16"}
+        ]},
+        {question: "When are cats most active?",
+        answers: [
+            {a: "Night"},
+            {b: "Afternoon"},
+            {c: "Dawn and Dusk", correct: true},
+            {d: "Mid-Morning"}
+        ]},
+        {question: "What do cats spend up to a third of their waking hours doing?",
+        answers: [
+            {a: "Eating"},
+            {b: "Playing"},
+            {c: "Staring Into Space"},
+            {d: "Grooming", correct: true}
+        ]},
+        {question: "What is a group of adult cats called?",
+        answers: [
+            {a: "Pride"},
+            {b: "Clowder", correct: true},
+            {c: "Pack"},
+            {d: "Catlet"}
+        ]},
+        {question: "How much of its life does the average cat spend sleeping?",
+        answers: [
+            {a: "90%"},
+            {b: "60%"},
+            {c: "33%"},
+            {d: "75%", correct: true}
+        ]},
+        {question: "What is one of the longest running Broadway shows of all time?",
+        answers: [
+            {a: "Cat on a Hot Tin Roof"},
+            {b: "Cats", correct: true},
+            {c: "The Aristocats"},
+            {d: "The Cat's Meow"}
+        ]},
+        {question: "How fast can the average cat run?",
+        answers: [
+            {a: "15mph"},
+            {b: "50mph"},
+            {c: "30mph", correct: true},
+            {d: "5mph"}
+        ]},
+        {question: "What kind of cat is believed to be good luck in Great Britain and Germany?",
+        answers: [
+            {a: "Black Cat", correct: true},
+            {b: "Orange Cat"},
+            {c: "Female Cat"},
+            {d: "Tabby Cat"}
+        ]},
+        {question: "Which ancient civilization worshipped cats?",
+        answers: [
+            {a: "Romans"},
+            {b: "Babylonians"},
+            {c: "Byzantines"},
+            {d: "Egyptians", correct: true}
+        ]},
+    ],
+    medium: [
+
+    ]
+}
